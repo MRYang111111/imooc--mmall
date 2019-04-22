0ceae3f2acc44d25d68c2e360a396bd19441c969
origin <git@github.com:MRYang111111/imooc--mmall.git>
2019-04-22 15:49:56 +0800

    second commit

---
+++ src/main/java/cn/yxd/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package cn.yxd.dao;
+
+import cn.yxd.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
